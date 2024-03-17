0e3327017bbab2f3a249e90023796595d466bebb
Anurag Malviya <anuragmalviya62@gmail.com>
2024-03-18 01:23:14 +0530

    middlewares add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 
 import connectDB from "./database/db.js";
 import dotenv from "dotenv";
-
+import {app} from "./app.js"
 dotenv.config({
     path: './env'
 });
-connectDB();
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 ,(req,res) => {
+      console.log(`Server running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+   console.log("MONGODB CONNECTION ERROR",err)
+})
---
+++ src/utills/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, {
+              resource_type: "auto"
+        })
+
+        console.log("file is uploaded on cloudinary",response.url);
+        return response;
+    } catch(error) {
+       fs.unlinkSync(localFilePath)
+       return null
+    }
+}
+
+
+export {uploadOnCloudinary}
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); }); 
